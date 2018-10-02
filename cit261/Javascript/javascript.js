b79e7f76b0fa66fae5131b2382b1cc937022c060
Paul Nielsen <pacanielsen@gmail.com>
2018-10-01 20:50:18 -0600

    forEach loop

--- cit261/Javascript/javascript.js
+++ cit261/Javascript/javascript.js
@@ -5,3 +5,15 @@ function forLoop() {
     }
     document.getElementById('forLoop').innerHTML = loopString;
 }
+
+function forEachLoop() {
+    var loopArray = array[10];
+    for(var i = 10; i < 21; i++) {
+        loopArray[i] = i;    
+    }
+    var forEachString = " ";
+    loopArray.forEach(function(element) {
+        forEachString += i + " ";
+    })
+    document.getElementById('forEachLoop').innerHTML = forEachString;
+}
