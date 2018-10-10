d4a52e918f0737634924f385721632d1ba0825ea
Paul Nielsen <pacanielsen@gmail.com>
2018-10-10 12:20:35 -0600

    unto a few

--- cit261/AJAX/AJAX.js
+++ cit261/AJAX/AJAX.js
@@ -3,20 +3,10 @@ function tryAJAX() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        var ajaxArray = JSON.parse(this.responseText);
-        print(ajaxArray);
+        document.getElementById("printAJAX").innerHTML =
+        this.responseText;
       }
     };
-
-    xhttp.open('GET', 'AJAX.txt', true);
+    xhttp.open('GET', 'smallAJAX.txt', true);
     xhttp.send();
-}
-
-function print(array) {
-    var out = "";
-    var i;
-    for(i = 0; i < array.length; i++) {
-        out += array[i].firstName + " " + array[i].lastName + " is " + array[i].age + " years old.";
-    }
-    document.getElementById('printAJAX').innerHTML = out;
 }
