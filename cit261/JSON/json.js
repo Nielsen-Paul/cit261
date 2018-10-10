8ce3037e8e59da9e4ee6ac5490b2031f47d7132f
Paul Nielsen <pacanielsen@gmail.com>
2018-10-10 09:57:05 -0600

    add JSON files

---
+++ cit261/JSON/json.js
@@ -0,0 +1,10 @@
+
+const player = {
+    name: 'Tom Brady',
+    team: 'Patriots',
+    number: 12
+}
+
+const playerObjToString = JSON.stringify(player);
+
+document.getElementById('printPlayer').innerHTML = myJSON;
