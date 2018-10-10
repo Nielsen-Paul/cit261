0f3fa23ee3ee096a8960cddbd623eef8c8a39351
Paul Nielsen <pacanielsen@gmail.com>
2018-10-10 12:25:51 -0600

    small text and array

--- cit261/AJAX/AJAX.js
+++ cit261/AJAX/AJAX.js
@@ -1,12 +1,41 @@
 
 function tryAJAX() {
+    const xhr = new MLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                document.getElementById('printAJAX').innerHTML = xhr.responseText;
+            }
+
+            if (xhr.status == 404) {
+                document.getElementById('printAJAX').innerHTML = "File not found";
+            }
+        }
+    };
+
+    xhr.open('get', 'smallAjax.txt', true);
+    xhr.send();
+}
+
+function tryAJAX2() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("printAJAX").innerHTML =
-        this.responseText;
+        var ajaxArray = JSON.parse(this.responseText);
+        print(ajaxArray);
       }
     };
-    xhttp.open('GET', 'smallAJAX.txt', true);
+
+    xhttp.open('GET', 'AJAX.txt', true);
     xhttp.send();
+}
+
+function print(array) {
+    var out = "";
+    var i;
+    for(i = 0; i < array.length; i++) {
+        out += array[i].firstName + " " + array[i].lastName + " is " + array[i].age + " years old.<br>";
+    }
+    document.getElementById('printAJAX2').innerHTML = out;
 }
