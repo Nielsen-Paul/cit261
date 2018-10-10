0eee9e8725eade815a242e19c25095b857603cf7
Paul Nielsen <pacanielsen@gmail.com>
2018-10-10 10:21:15 -0600

    string to JSON

--- cit261/JSON/json.js
+++ cit261/JSON/json.js
@@ -1,6 +1,4 @@
 
-
-
 function createPlayer() {
 
     var player = { name: 'Tom Brady', team: 'Patriots', number: 12 };
@@ -9,5 +7,14 @@ function createPlayer() {
 
     document.getElementById('printPlayer').innerHTML = playerObjToString;
 
+}
+
+function stringToJSON() {
+    var userString = document.getElementById('input').value;
+
+    var jsonObj = JSON.parse(userString);
+
+    document.getElementById('printPerson').innerHTML = 
+        jsonObj.firstName + " " + jsonObj.lastName + " is " + jsonObj.age + " years old."
 }
 
