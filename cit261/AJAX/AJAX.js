fd5929b510594cb17f93f8ae5718f0bb72fcddba
Paul Nielsen <pacanielsen@gmail.com>
2018-10-10 12:08:17 -0600

    file has an array now

--- cit261/AJAX/AJAX.js
+++ cit261/AJAX/AJAX.js
@@ -3,11 +3,20 @@ function tryAJAX() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("printAJAX").innerHTML =
-        this.responseText;
+        var ajaxArray = JSON.parse(this.responseText);
+        print(ajaxArray);
       }
     };
-    
+
     xhr.open('GET', 'AJAX.txt', true);
     xhr.send();
+}
+
+function print(array) {
+    var out = "";
+    var i;
+    for(i = 0; i < array.length; i++) {
+        out += jsonObj.firstName + " " + jsonObj.lastName + " is " + jsonObj.age + " years old.<br>";
+    }
+    document.getElementById('printAJAX').innerHTML = out;
 }
