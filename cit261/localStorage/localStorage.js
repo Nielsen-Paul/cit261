55d12df9395be58a37c90a64bac3e9180e842c6d
Paul Nielsen <pacanielsen@gmail.com>
2018-10-20 11:15:42 -0600

    change form

--- cit261/localStorage/localStorage.js
+++ cit261/localStorage/localStorage.js
@@ -3,7 +3,7 @@ var nameBttn = document.querySelector('name');
 
 function printName() {
     localStorage.setItem('name', 'Paul');
-    document.getElementById.innerHTML = localStorage.getItem('name');
+    document.getElementById('printName').innerHTML = localStorage.getItem('name');
 }
 
 nameBttn.addEventListener('click', printName);
