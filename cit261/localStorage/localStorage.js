a58482dfef1a81c3c50def496695e0f35e778e30
Paul Nielsen <pacanielsen@gmail.com>
2018-10-20 09:46:52 -0600

    make it my own

--- cit261/localStorage/localStorage.js
+++ cit261/localStorage/localStorage.js
@@ -1,12 +1,13 @@
 
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
-const button = document.querySelector('button');
-const input = document.getElementById('item');
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+const submit = document.querySelector('submit');
+const clear = document.querySelector('clear');
+const input = document.getElementById('name');
+let itemsArray = localStorage.getItem('name') ? JSON.parse(localStorage.getItem('name')) : [];
 
-localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
+localStorage.setItem('name', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('name'));
 
 const liMaker = (text) => {
   const li = document.createElement('li');
@@ -14,11 +15,11 @@ const liMaker = (text) => {
   ul.appendChild(li);
 }
 
-form.addEventListener('submit', function (e) {
+submit.addEventListener('click', function (e) {
   e.preventDefault();
 
   itemsArray.push(input.value);
-  localStorage.setItem('items', JSON.stringify(itemsArray));
+  localStorage.setItem('name', JSON.stringify(itemsArray));
   liMaker(input.value);
   input.value = "";
 });
@@ -27,7 +28,7 @@ data.forEach(item => {
   liMaker(item);
 });
 
-button.addEventListener('click', function () {
+clear.addEventListener('click', function () {
   localStorage.clear();
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
