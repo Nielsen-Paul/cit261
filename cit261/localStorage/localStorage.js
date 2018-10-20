f8bb4a4b20e4e18fd7e0a060feb2f154f105c56a
Paul Nielsen <pacanielsen@gmail.com>
2018-10-20 11:01:54 -0600

    change form

--- cit261/localStorage/localStorage.js
+++ cit261/localStorage/localStorage.js
@@ -2,7 +2,7 @@
 var nameForm = document.getElementById('nameForm');
 var name = document.getElementById('name');
 
-nameForm.onclick = function () {
+nameForm.onsubmit = function () {
 
     localStorage.setItem('name', name.value);
     document.getElementById('printName') = localStorage.getItem('name');
