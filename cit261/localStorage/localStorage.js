1f1876f3b9fda7d2d130fe866f9a6b5932d4c4b0
Paul Nielsen <pacanielsen@gmail.com>
2018-10-20 11:33:38 -0600

    add form

--- cit261/localStorage/localStorage.js
+++ cit261/localStorage/localStorage.js
@@ -6,4 +6,10 @@ function printName() {
     document.getElementById('printName').innerHTML = localStorage.getItem('name');
 }
 
+function printBirthMonth() {
+    var birthMonth = getElementById('birthMonth');
+    localStorage.setItem('birthMonth', birthMonth.value);
+    document.getElementById('printBirthMonth').innerHTML = localStorage.getItem('birthMonth');
+}
+
 nameBttn.addEventListener('click', printName);
