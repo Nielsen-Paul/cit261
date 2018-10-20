f364ca80501adac68bba278bfe3c7c3c717fd1cd
Paul Nielsen <pacanielsen@gmail.com>
2018-10-20 10:35:40 -0600

    new direction

--- cit261/localStorage/localStorage.js
+++ cit261/localStorage/localStorage.js
@@ -1,35 +1,15 @@
 
-const form = document.querySelector('form');
-const ul = document.querySelector('ul');
-const button = document.querySelector('button');
-const input = document.getElementById('name');
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+var nameForm = document.querySelector('nameForm');
+var name = document.getElementById('name');
+var data = localStorage.getItem('name');
 
-localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
+nameForm.addEventListener('click', function () {
 
-const liMaker = (text) => {
-    const li = document.createElement('li');
-    li.textContent = text;
-    ul.appendChild(li);
-}
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    itemsArray.push(input.value);
-    localStorage.setItem('items', JSON.stringify(itemsArray));
-    liMaker(input.value);
-    input.value = "";
+    localStorage.setItem('name', name.value);
+    document.getElementById('printName') = localStorage.getItem('name');
+    
 });
-
-data.forEach(item => {
-    liMaker(item);
-})
 
 button.addEventListener('click', function () {
     localStorage.clear();
-    while (ul.firstChild) {
-        ul.removeChild(ul.firstChild);
-    }
 });
