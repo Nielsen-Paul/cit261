384eb9906aa2de8da54701d6b6d9fbdfd847b1bf
Paul Nielsen <pacanielsen@gmail.com>
2018-10-22 21:22:34 -0600

    DOM

--- cit261/DOM/DOM.js
+++ cit261/DOM/DOM.js
@@ -1,6 +1,15 @@
 
-function deleteMe () {
+function deleteMe() {
     var parent = document.getElementById("div1");
     var child = document.getElementById("button1");
     parent.removeChild(child);
+}
+
+function createSibling() {
+    var button = document.createElement("button");
+    button.value("Change me");
+
+    var element = document.getElementById("div1");
+    var child = document.getElementById("button2");
+    element.insertBefore(button, child);
 }
