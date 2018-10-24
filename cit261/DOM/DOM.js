6ffb52229de94769633e2e5fe16e8a887f65455c
Paul Nielsen <pacanielsen@gmail.com>
2018-10-24 14:52:30 -0600

    create leapfrog

--- cit261/DOM/DOM.js
+++ cit261/DOM/DOM.js
@@ -21,7 +21,7 @@ function leaper() {
 
     var button2 = document.createElement("button");
     var value2 = document.createTextNode("Leap")
-    button2.attachEvent('onclick', leaper);
+    button2.addEventListener('onclick', leaper);
 
     button2.appendChild(value2);
     document.body.appendChild(button2);
