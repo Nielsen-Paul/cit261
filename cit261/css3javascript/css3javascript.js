5ca9e723f287fcbfaba32dd2fd623a4ff6565995
Paul Nielsen <pacanielsen@gmail.com>
2018-10-27 11:15:47 -0600

    textTransform

--- cit261/css3javascript/css3javascript.js
+++ cit261/css3javascript/css3javascript.js
@@ -3,3 +3,20 @@ document.getElementById('id1').style.color = 'red'
 function changeBackground() {
     document.getElementById('div1').style.background = 'blue';
 }
+
+function changeText() {
+    document.getElementById('div2').style.textDecoration = 'overline';
+    document.getElementById('div2').style.textTransform = 'uppercase';
+}
+
+function changeFont() {
+    document.getElementById('div1').style.background = 'blue';
+}
+
+function changeShadow() {
+    document.getElementById('div1').style.background = 'blue';
+}
+
+function changeBox() {
+    document.getElementById('div1').style.background = 'blue';
+}
