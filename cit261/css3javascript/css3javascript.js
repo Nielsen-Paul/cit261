b71ae17e8c7db168d132fcdbf5dbb146fd5c5339
Paul Nielsen <pacanielsen@gmail.com>
2018-10-27 11:41:58 -0600

    change box

--- cit261/css3javascript/css3javascript.js
+++ cit261/css3javascript/css3javascript.js
@@ -26,5 +26,5 @@ function changeBox() {
 }
 
 function changeBody() {
-    document.getElementById('body').style.background = 'orange';
+    document.getElementsByName('body').style.background = 'orange';
 }
