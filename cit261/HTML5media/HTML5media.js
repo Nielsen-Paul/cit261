44edc34b8ed2bb099f77f25f290c90731a3bb7fe
Paul Nielsen <pacanielsen@gmail.com>
2018-10-30 17:20:05 -0600

    add box canvas

--- cit261/HTML5media/HTML5media.js
+++ cit261/HTML5media/HTML5media.js
@@ -1,8 +1,15 @@
 
-window.onload = function() {
+window.onload = function () {
     document.getElementById('campbells').style.display = none;
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     var img = document.getElementById("campbells");
     ctx.drawImage(img, 10, 10);
+
+    var canvas = document.getElementById('boxCanvas');
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(100, 100, 100, 100);
 };
+
