16cace80e2b7644236cbe36120415b18c9241cd2
Paul Nielsen <pacanielsen@gmail.com>
2018-10-31 13:59:03 -0600

    move box

--- cit261/HTML5media/HTML5media.js
+++ cit261/HTML5media/HTML5media.js
@@ -11,9 +11,22 @@ window.onload = function () {
 window.onload = function () {
     var canvas2 = document.getElementById('boxCanvas');
     var ctx2 = canvas2.getContext('2d');
-    
+
     ctx2.fillStyle = 'blue';
     ctx2.fillRect(100, 100, 100, 100);
-    
+
 };
+
+function moveBox() {
+    var parent = document.getElementById("div4");
+    var canvas3 = document.createElement("canvas");
+    var ctx3 = canvas3.getContext('2d');
+
+    ctx3.fillStyle = 'blue';
+    ctx3.fillRect(100, 10, 100, 100);
+
+    var canvas4 = document.getElementById("boxCanvas");
+
+    parent.replaceChild(canvas3, canvas4);
+}
 
