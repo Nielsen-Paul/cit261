2560e1047b868339d86f68270ab6de55901b0b72
Paul Nielsen <pacanielsen@gmail.com>
2018-11-15 17:12:21 -0700

    ontouchmove

--- cit261/javascriptEvents/javascriptEvents.js
+++ cit261/javascriptEvents/javascriptEvents.js
@@ -10,6 +10,10 @@ window.onload=function(){
     var box = document.getElementById("spinningBox");
     box.addEventListener("touchstart", spinBox);
     document.getElementById("doNotMove").ontouchmove = doNotMove;
+    var leap = document.getElementById("leap");
+    leap.addEventListener("click", leaper);
+    var frog = document.getElementById("frog");
+    frog.addEventListener("click", frogger);
 }
 
 function spinBox() {
@@ -20,4 +24,17 @@ function spinBox() {
 
 function doNotMove() {
     document.getElementById("doNotMove").innerHTML = "Rebel!!!"
+}
+
+function leaper() {
+    var parent = document.getElementById("leapFrog");
+    var child = document.getElementById("leap");
+    parent2.removeChild(child);
+
+    var button = document.createElement("button");
+    var value = document.createTextNode("Leap");
+
+    button.appendChild(value);
+    button.addEventListener("click", leaper);
+    document.body.appendChild(button);
 }
