7e70ec6e06f3250cf7dba61e60081c85fa31eca7
Paul Nielsen <pacanielsen@gmail.com>
2018-11-30 16:13:56 -0700

    local storage

---
+++ cit261/CS261Project/cs261project.js
@@ -0,0 +1,17 @@
+
+function taxQuote() {
+    var name = document.getElementById('name').value;
+    var age = document.getElementById('age').value;
+    var income = document.getElementById('income').value;
+    var familySize = document.getElementById('familySize').value;
+
+    localStorage.setItem("name", name);
+    localStorage.setItem("age", age);
+    localStorage.setItem("income", income);
+    localStorage.setItem("familySize", familySize);
+}
+
+function printStorage() {
+    document.getElementById('printName').innerHTML = 
+    "Welcome back " + localStorage.getItem('name') + "!";
+}
