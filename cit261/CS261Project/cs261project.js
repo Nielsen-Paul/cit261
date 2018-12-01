f389a3385a0d9b6e63e8ee2985904bb69c6d5e49
Paul Nielsen <pacanielsen@gmail.com>
2018-11-30 17:24:45 -0700

    local storage

--- cit261/CS261Project/cs261project.js
+++ cit261/CS261Project/cs261project.js
@@ -1,10 +1,17 @@
 
 window.onload = function () {
     document.getElementById('printName').innerHTML = "Welcome back " + localStorage.getItem("name");
+
     document.getElementById('name').value = localStorage.getItem("name");
     document.getElementById('age').value = localStorage.getItem("age");
     document.getElementById('income').value = localStorage.getItem("income");
     document.getElementById('familySize').value = localStorage.getItem("familySize");
+
+    document.getElementById('pocketName').value = localStorage.getItem("name");
+    document.getElementById('pocketAge').value = localStorage.getItem("age");
+    document.getElementById('pocketIncome').value = localStorage.getItem("income");
+    document.getElementById('pocketFamilySize').value = localStorage.getItem("familySize");
+
     document.getElementById('hiddenTaxDiv').style.display = none;
     document.getElementById('hiddenMedicareDiv').style.display = none;
     document.getElementById('hiddenPocketDiv').style.display = none;
