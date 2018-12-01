e6d0d815e0a32fb5db055bbd21c14ac82fd25825
Paul Nielsen <pacanielsen@gmail.com>
2018-11-30 17:11:37 -0700

    local storage

--- cit261/CS261Project/cs261project.js
+++ cit261/CS261Project/cs261project.js
@@ -5,6 +5,7 @@ window.onload = function () {
     document.getElementById('age').value = localStorage.getItem("age");
     document.getElementById('income').value = localStorage.getItem("income");
     document.getElementById('familySize').value = localStorage.getItem("familySize");
+    document.getElementById('insuranceEligible').value = localStorage.getItem("insuranceEligible");
     document.getElementById('hiddenTaxDiv').style.display = none;
     document.getElementById('hiddenMedicareDiv').style.display = none;
     document.getElementById('hiddenPocketDiv').style.display = none;
@@ -15,10 +16,12 @@ function taxQuote() {
     var age = document.getElementById('age').value;
     var income = document.getElementById('income').value;
     var familySize = document.getElementById('familySize').value;
+    var insuranceEligible = document.getElementById('insuranceEligible').value;
 
     localStorage.setItem("name", name);
     localStorage.setItem("age", age);
     localStorage.setItem("income", income);
     localStorage.setItem("familySize", familySize);
+    localStorage.setItem("insuranceEligible", insuranceEligible);
 }
 
