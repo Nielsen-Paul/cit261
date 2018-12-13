e124d4e277ebf261c8c8166a2f6e39f544e50a2d
Paul Nielsen <pacanielsen@gmail.com>
2018-12-13 14:24:03 -0700

    add css

--- cit261/CS261Project/cs261project.js
+++ cit261/CS261Project/cs261project.js
@@ -18,23 +18,26 @@ window.onload = function () {
     document.getElementById('hiddenTaxEligible').style.display = "none";
     document.getElementById('hiddenMedicareEligible').style.display = "none";
 
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var myArr = JSON.parse(this.responseText);
-            var age = localStorage.getItem("age") - 20;
-            var familySize = localStorage.getItem("familySize");
-            var bronze = myArr.medal[0] * myArr.amounts[age] + myArr.family[familySize - 1];
-            var silver = myArr.medal[1] * myArr.amounts[age] + myArr.family[familySize - 1];
-            var gold = myArr.medal[2] * myArr.amounts[age] + myArr.family[familySize - 1];
-            document.getElementById("ajaxTest").innerHTML = 
-            "At " + myArr.ages[age] + " years old and with a family size of " +
-            familySize + " here are the monthly payment options: <br>" + 
-            "Bronze: " + bronze + " Silver: " + silver + " Gold: " + gold;
-        }
-    };
-    xmlhttp.open("GET", "payments.txt", true);
-    xmlhttp.send();
+    if (!localStorage.getItem(quoteFile)) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var myArr = JSON.parse(this.responseText);
+                localStorage.setItem(quoteFile, myArr);
+                var age = localStorage.getItem("age") - 20;
+                var familySize = localStorage.getItem("familySize");
+                var bronze = myArr.medal[0] * myArr.amounts[age] + myArr.family[familySize - 1];
+                var silver = myArr.medal[1] * myArr.amounts[age] + myArr.family[familySize - 1];
+                var gold = myArr.medal[2] * myArr.amounts[age] + myArr.family[familySize - 1];
+                document.getElementById("ajaxTest").innerHTML =
+                    "At " + myArr.ages[age] + " years old and with a family size of " +
+                    familySize + " here are the monthly payment options: <br>" +
+                    "Bronze: " + bronze + " Silver: " + silver + " Gold: " + gold;
+            }
+        };
+        xmlhttp.open("GET", "payments.txt", true);
+        xmlhttp.send();
+    }
 
 }
 
